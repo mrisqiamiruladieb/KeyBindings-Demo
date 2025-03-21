1d1dc3154f933feac4bf622ab5e32617724e4fd7
adieb <mrizqiamiruladieb@gmail.com>
2025-03-21 17:23:11 +0700

    feat: define objects

---
+++ src/Game.java
@@ -0,0 +1,55 @@
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Game {
+    
+    //declare
+    JFrame frame;
+    JLabel label;
+    Action upAction;
+    Action downAction;
+    Action leftAction;
+    Action rightAction;
+    
+    //construct
+    Game() {
+        
+    }
+    
+    public class upAction extends AbstractAction {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            
+        }
+        
+    }
+    
+    public class downAction extends AbstractAction {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            
+        }
+        
+    }
+    
+    public class leftAction extends AbstractAction {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            
+        }
+        
+    }
+    
+    public class rightAction extends AbstractAction {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            
+        }
+        
+    }
+    
+}
